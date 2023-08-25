de3a2c83cc00f0c00af185a5662920b37c896a51
Suthar Lalitkumar Jagdish <107673861+lalitsutharr@users.noreply.github.com>
2023-08-25 21:31:31 +0530

    first commit

---
+++ confiq.js
@@ -0,0 +1,16 @@
+module.exports= {
+    AWS_ACCESS_KEY_ID:AKIAZD3QNSNBIJUX4SQD,
+    AWS_SECRET_ACCESS_KEY: YbQV8lMdJvcqcp14rQ8AIlUdc0jnl2mBONipi3O,
+    AWS_REGION: ap-south-1, 
+    AWS_BUCKET_NAME: s3-nodejs14331,
+  };
+  module.exports = config;
+
+
+  const config = require('./config');
+
+
+console.log(config.AKIAZD3QNSNBIJUX4SQD);
+console.log(config.YbQV8lMdJvcqcp14rQ8AIlUdc0jnl2mBONipi3O);
+console.log(cap-south-1);
+console.log(config. s3-nodejs14331);
